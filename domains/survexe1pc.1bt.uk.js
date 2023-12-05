637f94b9675fbcb9aa753be7e9441abf64860c1e
https://github.com/Ashimee <153018070+SurvExe1Pc@users.noreply.github.com>
2023-12-05 14:13:12 -0500

    Create survexe1pc.1bt.uk.js

---
+++ domains/survexe1pc.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'My website redirectionn',
+  domain: '1bt.uk',
+  subdomain: 'survexe1pc',
+  owner: {
+    repo: 'https://github.com/SurvExE1Pc/survexe1pc.github.io',
+    email: 'ashimesugar@gmail.com',
+  },
+  record: {
+    CNAME: 'survexe1pc.github.io',
+  },
+})
